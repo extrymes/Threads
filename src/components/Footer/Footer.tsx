e031cf8a788c5ba9499c9752d010941f7a8baa89
extrymes <65893656+extrymes@users.noreply.github.com>
2025-02-03 21:11:22 +0100

    feat(footer): add footer component with copyright and terms

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="flex z-30 justify-center gap-10 pb-4 mt-10 text-threads-gray-light">
+      <div>© Threads</div>
+      <div>General terms and conditions</div>
+      <div>Privacy policy</div>
+    </footer>
+  )
+}

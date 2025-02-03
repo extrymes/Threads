d947055646d32cdfe87892aa0502b8f4412207c5
extrymes <65893656+extrymes@users.noreply.github.com>
2025-02-03 21:10:06 +0100

    feat(button): add form button support with loading state

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+export default function Button({
+  children,
+  onClick,
+  withoutMarginTop,
+  formButton,
+}: {
+  children: React.ReactNode;
+  onClick?: () => void;
+  withoutMarginTop?: boolean;
+  formButton?: boolean;
+}) {
+  const { pending } = useFormStatus();
+  return (
+    <div>
+      <button
+        disabled={formButton && pending}
+        className={`bg-white rounded-3xl border-threads-gray-light w-full p-4 ${
+          withoutMarginTop ? "" : `mt-4`
+        } hover:bg-gray-300 disabled:bg-opacity-50 disabled:cursor-not-allowed duration-150`}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </div>
+  );
+}

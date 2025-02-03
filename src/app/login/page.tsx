1d774dc0ec4c99b7ed569da1b3fd81d60adccb78
extrymes <65893656+extrymes@users.noreply.github.com>
2025-02-03 21:17:18 +0100

    feat(auth): add login page with guest and email options

---
+++ src/app/login/page.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <div>
+      {/* Title */}
+      <h1 className="title pt-[19vw]">
+        How do you want to use Threads?
+      </h1>
+
+      <div className="mt-5 w-[500px] mx-auto flex flex-col gap-4">
+        {/* Sign up and sign in */}
+        <Link href="/login/signup">
+          <div className="auth-method">
+            <h2 className="font-bold text-white">
+              Sign up or sign in with an email address
+            </h2>
+            <div className="text-threads-gray-light mt-4">
+              Sign in or create your Threads profile with an email address. This
+              will allow you to post and interact on Threads.
+            </div>
+          </div>
+        </Link>
+
+        {/* Invited */}
+        <Link href="/login/pass">
+          <div className="auth-method">
+            <h2 className="font-bold text-white">Continue as guest</h2>
+            <div className="text-threads-gray-light mt-4">
+              You can navigate on Threads without profile, but you cannot post
+              and interact with content.
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}

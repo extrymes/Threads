3bffa054a759b9701459ee4bc81799b200b5c24e
extrymes <65893656+extrymes@users.noreply.github.com>
2025-02-04 07:24:24 +0100

    feat(auth): implement session provider for authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { ToastContainer } from "react-toastify";
 import "./globals.css";
+import { AuthProvider } from "./Providers";
 
 export const metadata: Metadata = {
   title: "Threads",
@@ -15,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-threads-gray">
-        {children}
+        <AuthProvider>{children}</AuthProvider>
         <ToastContainer position="bottom-right" />
       </body>
     </html>

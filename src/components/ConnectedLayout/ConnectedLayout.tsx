6d0a2b9bc7dc08ddc171907c6df98da64eb41af3
extrymes <65893656+extrymes@users.noreply.github.com>
2025-02-05 09:32:51 +0100

    feat(layout): add profile section with dropdown menu in header

--- src/components/ConnectedLayout/ConnectedLayout.tsx
+++ src/components/ConnectedLayout/ConnectedLayout.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
 import Button from "../Button/Button";
+import Dropwdown from "../Dropdown/Dropwdown";
 import Footer from "../Footer/Footer";
 
 export default function ConnectedLayout({
@@ -11,7 +14,12 @@ export default function ConnectedLayout({
 }: {
   children: React.ReactNode;
 }) {
+  // Variables
   const pathname = usePathname();
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [dropdownIsOpen, dropdownSetIsOpen] = useState(false);
+
   return (
     <section className="flex flex-col min-h-screen px-5">
       {/* Header */}
@@ -58,11 +66,65 @@ export default function ConnectedLayout({
         {/* Logo */}
         <Image src="/logo.png" alt="Threads" priority width={40} height={40} />
 
-        {/* Button */}
-        <div className="z-10">
-          <Link href="/login">
-            <Button withoutMarginTop>Log in</Button>
-          </Link>
+        {/* Profile */}
+        <div className="z-10 relative inline-block text-left">
+          {session?.user ? (
+            // User avatar
+            <Image
+              onClick={() => dropdownSetIsOpen(!dropdownIsOpen)}
+              src={session.user.profile}
+              alt="Profile"
+              width={60}
+              height={60}
+              className="cursor-pointer rounded-full border-4 border-transparent hover:border-threads-gray-light duration-150"
+            ></Image>
+          ) : (
+            // Login button
+            <Link href="/login">
+              <Button withoutMarginTop>Login</Button>
+            </Link>
+          )}
+
+          {/* Dropdown menu */}
+          <Dropwdown isOpen={dropdownIsOpen} setIsOpen={dropdownSetIsOpen}>
+            {/* My profile option */}
+            <Link href={`/@${session?.user.username}`}>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="mr-1"
+                  width={22}
+                  height={22}
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M12 12q-1.65 0-2.825-1.175T8 8t1.175-2.825T12 4t2.825 1.175T16 8t-1.175 2.825T12 12m-8 8v-2.8q0-.85.438-1.562T5.6 14.55q1.55-.775 3.15-1.162T12 13t3.25.388t3.15 1.162q.725.375 1.163 1.088T20 17.2V20z"
+                  ></path>
+                </svg>
+                My profile
+              </li>
+            </Link>
+
+            {/* Logout option */}
+            <li onClick={() => signOut()}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="mr-1 text-red-500"
+                width={22}
+                height={22}
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  fillRule="evenodd"
+                  d="m8.379 2.669l3.69-.58c3.22-.506 4.83-.76 5.88.139C19 3.126 19 4.756 19 8.016V11h-5.92l2.7-3.376l-1.56-1.25l-4 5l-.5.626l.5.624l4 5l1.56-1.25L13.08 13H19v2.983c0 3.26 0 4.89-1.05 5.788s-2.66.645-5.881.14l-3.69-.58c-1.613-.254-2.419-.38-2.899-.942S5 19.012 5 17.38V6.62c0-1.632 0-2.449.48-3.01s1.286-.688 2.899-.941"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              Logout
+            </li>
+          </Dropwdown>
         </div>
       </header>
 

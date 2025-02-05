d076ef250de72b6ce9d95fd8a4551a7ce5dc298a
extrymes <65893656+extrymes@users.noreply.github.com>
2025-02-05 21:26:08 +0100

    feat(post): implement MongoDB post creation functionality

---
+++ src/actions/create-post.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { MongoClient } from "mongodb";
+
+export const createPost = async (content: string, username: string, profile: string) => {
+  // Connect to the MongoDB cluster
+  const client = await MongoClient.connect(
+    process.env.MONGODB_CLIENT as string
+  );
+  // Connect to the database
+  const db = client.db(process.env.MONGODB_DATABASE);
+  // Create post in database
+  await db.collection("posts").insertOne({
+    content,
+    username,
+    profile,
+    creation: new Date(),
+  });
+  // Close MongoDB connection
+  await client.close();
+};

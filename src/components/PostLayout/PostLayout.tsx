a5b424bc10ca2727ce5d249a249bacb49ede3e08
extrymes <65893656+extrymes@users.noreply.github.com>
2025-02-06 22:50:13 +0100

    feat(post): add post options dropdown menu in `PostLayout` component

--- src/components/PostLayout/PostLayout.tsx
+++ src/components/PostLayout/PostLayout.tsx
@@ -1,12 +1,24 @@
+"use client";
+
 import { Post } from "@/types/Post";
 import moment from "moment-timezone";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import Dropwdown from "../Dropdown/Dropwdown";
 
 export default function PostLayout({ post }: { post: Post }) {
+  // Variables
+  const { data: session } = useSession();
+
+  // State management
+  const [openDropdown, setOpenDropdown] = useState(false);
+
+  // Render
   return (
     <div className="post">
-      {/* Avatar */}
+      {/* Profile */}
       <div>
         <Image
           src={post.profile}
@@ -17,18 +29,89 @@ export default function PostLayout({ post }: { post: Post }) {
         />
       </div>
 
-      {/* Content */}
+      {/* Post content */}
       <div className="text-white w-full">
-        {/* Infos */}
         <div className="flex justify-between text-center">
+          {/* Username */}
           <Link href={`/@${post.username}`}>
             <b>{post.username}</b>
           </Link>
-          <div className="text-sm text-threads-gray-light">
-            {moment
-              .utc(post.creation, "YYYY-MM-DD HH:mm:ss")
-              .tz("Europe/Paris")
-              .fromNow()}
+
+          <div className="flex items-center gap-2 text-sm text-threads-gray-light relative">
+            {/* Creation date */}
+            <div>
+              {moment
+                .utc(post.creation, "YYYY-MM-DD HH:mm:ss")
+                .tz("Europe/Paris")
+                .fromNow()}
+            </div>
+
+            {/* Options */}
+            {session?.user && (
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1.2em"
+                  height="1.2em"
+                  className="cursor-pointer"
+                  viewBox="0 0 24 24"
+                  onClick={() => setOpenDropdown((state) => !state)}
+                >
+                  <path
+                    fill="currentColor"
+                    d="M7 12a2 2 0 1 1-4 0a2 2 0 0 1 4 0m7 0a2 2 0 1 1-4 0a2 2 0 0 1 4 0m7 0a2 2 0 1 1-4 0a2 2 0 0 1 4 0"
+                  ></path>
+                </svg>
+              </div>
+            )}
+
+            {/* Dropdown menu */}
+            {session?.user && (
+              <Dropwdown
+                openDropdown={openDropdown}
+                setOpenDropdown={setOpenDropdown}
+                className="right-0 top-8"
+              >
+                {session.user.username != post.username ? (
+                  <li className="danger">
+                    Report
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                      <path
+                        fill="currentColor"
+                        d="M5 21V4h9l.4 2H20v10h-7l-.4-2H7v7z"
+                      ></path>
+                    </svg>
+                  </li>
+                ) : (
+                  <>
+                    <li>
+                      Edit
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1q-.15.15-.15.36M20.71 5.63l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83l3.75 3.75l1.83-1.83a.996.996 0 0 0 0-1.41"
+                        ></path>
+                      </svg>
+                    </li>
+                    <li className="danger">
+                      Delete
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z"
+                        ></path>
+                      </svg>
+                    </li>
+                  </>
+                )}
+              </Dropwdown>
+            )}
           </div>
         </div>
 

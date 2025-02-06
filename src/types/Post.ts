faacd8c8508055ba760894c04c499e6e9cbddab9
extrymes <65893656+extrymes@users.noreply.github.com>
2025-02-06 08:55:39 +0100

    feat(types): add User and Post type interfaces

---
+++ src/types/Post.ts
@@ -0,0 +1,7 @@
+export interface Post {
+  _id: string;
+  content: string;
+  username: string;
+  profile: string;
+  creation: Date;
+}
---
+++ src/types/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+  _id: string;
+  name: string;
+  username: string;
+  email: string;
+  profile: string;
+  bio: string;
+  url: string;
+  creation: Date;
+}

08d5bb86806ca45b51471ddb84653e75f6ae97c5
extrymes <65893656+extrymes@users.noreply.github.com>
2025-02-06 07:21:26 +0100

    fix(create-post): revalidate path after post creation

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,8 +1,13 @@
 "use server";
 
 import { MongoClient } from "mongodb";
+import { revalidatePath } from "next/cache";
 
-export const createPost = async (content: string, username: string, profile: string) => {
+export const createPost = async (
+  content: string,
+  username: string,
+  profile: string
+) => {
   // Connect to the MongoDB cluster
   const client = await MongoClient.connect(
     process.env.MONGODB_CLIENT as string
@@ -18,4 +23,6 @@ export const createPost = async (content: string, username: string, profile: str
   });
   // Close MongoDB connection
   await client.close();
+  // Revalidate path
+  revalidatePath("/");
 };

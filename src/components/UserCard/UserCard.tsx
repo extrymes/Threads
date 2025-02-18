f8aaba66f01fb312096bf8a26b76e89d48e2bf7c
extrymes <65893656+extrymes@users.noreply.github.com>
2025-02-18 15:34:49 +0100

    feat(components): create `UserCard` component for displaying user profile information

---
+++ src/components/UserCard/UserCard.tsx
@@ -0,0 +1,30 @@
+import { User } from "@/types/User";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function UserCard({ user }: { user: User }) {
+  // Render
+  return (
+    <Link className="user-card" href={`/@${user.username}`}>
+      {/* User avatar */}
+      <div className="shrink-0">
+        <Image
+          src={user.profile}
+          alt="User"
+          width={50}
+          height={50}
+          className="rounded-full object-cover aspect-square"
+          unoptimized
+        />
+      </div>
+      {/* User informations */}
+      <div className="text-white flex flex-col gap-2">
+        <div className="flex gap-2">
+          <b>{user.name}</b>
+          <p className="text-threads-gray-light">@{user.username}</p>
+        </div>
+        {user.bio && <p>{user.bio}</p>}
+      </div>
+    </Link>
+  );
+}
